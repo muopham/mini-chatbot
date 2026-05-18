f5a728af80f60a6456523f75990d4c3ff4606615
muopham <muopham5116@gmail.com>
2026-05-18 11:35:46 +0700

    fix bug deloy

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -43,17 +43,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="light" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          rel="stylesheet"
-        />
-
         <script
           dangerouslySetInnerHTML={{
             __html: `
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
           Page Not Found
         </p>
         <p className="mb-8 text-sm font-bold text-stone-500">
-          The page you're looking for doesn't exist or has been moved.
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
         <Link
           href="/"
